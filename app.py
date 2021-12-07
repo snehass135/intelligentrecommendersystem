1dfc172ea301c71c7cc225179df87280bffa5fa8
snehass135 <72939802+snehass135@users.noreply.github.com>
2021-12-07 21:12:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 import pickle as pk
 import pandas as pd
-Book_dict = pk.load(open('Books_dict.pkl', 'rb'))
+Book_dict = pk.load(open('Books_dict.pkl','rb'))
 Books = pd.DataFrame(Book_dict)
 def values():
     Auth = Author.get()
-    Au_name = Books[Books.Author_name == Auth].Book_name.values[1:6]
-    book_list = list(Au_name)
+    Au_name= Books[Books.Author_name == Auth].Book_name.values[1:6]
+    book_list=list(Au_name)
     for i in book_list:
         print(i)
-window = Tk()
+window = tk.Tk()
 window.geometry('1080x1080')
-Label(text="Book Reccomondation system", font=("Times New Roman", 20)).pack()
-tk.Label(window, text="Select Author to recommend: ",font=("Times New Roman", 15)).grid(column=0,row=15, padx=10, pady=25)
+window["bg"] = "cyan"
+Label(text="Book Recommender System", font= ("Times New Roman", 20)).grid()
+ttk.Label(window, text = "Select Author to recommend: ",font = ("Times New Roman", 10)).grid(column = 0,row = 15, padx = 10, pady = 25)
 n = tk.StringVar()
-Author = tk.Combobox(window, width=27, textvariable=n)
+Author = ttk.Combobox(window, width = 27,textvariable = n)
 # Adding combobox drop down list
-Author['values'] = ('Sidney Sheldon', 'Chetan Bhagat', 'Agatha Christe', 'Barbara Castland', 'Danielle Steel', 'Enid Blyton', 'J.K Rowling')
-Author.grid(column=1, row=15)
-Button(text='submit', command=values).pack()
+Author['values'] = ('Sidney Sheldon', 'Chetan Bhagat', 'Agatha Christe','Barbara Castland','Danielle Steel','Enid Blyton','J.K Rowling')
+Author.grid(column = 1, row = 15)
+Button(text = 'submit' , command=values).grid()
 window.mainloop()
-
