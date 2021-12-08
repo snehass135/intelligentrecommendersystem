6b122adc216a9cd5611f5a0939ce4b6746f5c12c
snehass135 <72939802+snehass135@users.noreply.github.com>
2021-12-08 19:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ from tkinter import *
 from tkinter import ttk
 import pickle as pk     #importing pickle to convert object hierarchy into data stream and vice-versa
 import pandas as pd     #importing pandas for dataframe
+
 Book_dict = pk.load(open('Books_dict.pkl','rb'))  #convert data stream to object hierarchy
 Books = pd.DataFrame(Book_dict)
+
+root = tk.Tk()
 def values():  #values are defined to print book list
+    win2 = Toplevel(root)
+    win2["bg"]="yellow"
+    win2.geometry('300x400')
+    win2.resizable(False,False)
     Auth = Author.get()
     Au_name = Books[Books.Author_name == Auth].Book_name.values[1:6]
     book_list = list(Au_name)
+    Label(win2, text="Recommended Books", font="Arial 12 bold", bg="black",fg="white").pack(padx=40, pady=10)
     for i in book_list:
-        print(i)
-root = tk.Tk()   #starting mainloop
-root.geometry('1080x1080')   #size for tkinter window is 1080 x 1080
+        Label(win2,text=i,font="Arial 12 bold",bg="yellow").pack()
+        #print(i)
+
+root.geometry('600x400')   #size for tkinter window is 1080 x 1080
 root["bg"] = "cyan"  #background colour for tkinter
 l1= Label(text="Book Recommender System", font= ("Times New Roman", 20))   #1st label is created
 l1.grid()
